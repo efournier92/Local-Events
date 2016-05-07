3d400c66e154e511be511fb2b1a8f020f6a95f62
Misigno <efournier92@gmail.com>
2016-05-07 15:50:11 -0400

    add README

--- app.rb
+++ app.rb
@@ -1,18 +1,23 @@
 require 'httparty'
-require 'pry'
 
-today = Date.today.to_s
+SEP = '***********************************************'
+
+today    = Date.today.to_s
 tomorrow = Date.today + 1
 tomorrow = tomorrow.to_s
 
+puts SEP
 print "Which City Do You Live In?\n>> "
 city = gets.chomp
+puts SEP
 
 url = "https://api.seatgeek.com/2/events\?venue.city\=#{city}\&datetime_utc.gte\=
   #{today}\&datetime_utc.lte\=#{tomorrow}"
-response    = HTTParty.get(url)
+response  = HTTParty.get(url)
 json_hash = response.parsed_response
 
 json_hash["events"].each do |event|
   puts event["title"]
 end
+
+puts SEP
